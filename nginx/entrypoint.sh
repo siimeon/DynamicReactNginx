00003bb36547fff895912b1267e730a914b3e7ae
Simo Haakana <simo.haakana@wunderdog.fi>
2018-07-26 18:25:57 +0300

    Sed version of dynamic environment variables

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,28 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
-ENV_STR=''
-
-list_env_var_names() {
-  ALL_ENV=$(env | sed -e 's/=.*//g')
-  for var in $ALL_ENV
+sed_replace_nginx_conf() {
+  template="/etc/nginx/conf.d/default.conf"
+  echo "For file ${template}:"
+  for var in $(env)
   do
-    ENV_STR="$ENV_STR"" \$\$""$var"
+    IFS="=" read -r key val <<< "$var"
+    if grep -q "\${${key}}" "$template"
+    then
+      echo "- replacing ${key} with ${val}"
+      sed -i "s/\${${key}}/${val}/g" $template
+    fi
   done
 }
 
-nginx_conf_envsubst() {
-  envsubst "$ENV_STR" < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+sed_replace_main_js() {
+  main_js=$(find /usr/share/nginx/html/static/js/main.*js)
+  echo "For file ${main_js}:"
+  for var in $(env)
+  do
+    IFS="=" read -r key val <<< "$var"
+    if grep -q "%%variable%%${key}%%variable%%" "$main_js"
+    then
+      echo "- replacing ${key} with ${val}"
+      sed -i "s#%%variable%%${key}%%variable%%#${val}#g" "$main_js"
+    fi
+  done
 }
 
-react_main_js_envsubst() {
-  MAIN_JS=$(find /usr/share/nginx/html/static/js/main.*js)
-  cp "$MAIN_JS" "$MAIN_JS".copy
-  envsubst "$ENV_STR" < "$MAIN_JS".copy > "$MAIN_JS"
-  rm "$MAIN_JS".copy
-}
-
-list_env_var_names
-nginx_conf_envsubst
-react_main_js_envsubst
+echo "Configuring nginx"
+sed_replace_nginx_conf
+echo "Configuring application"
+sed_replace_main_js
+echo "Starting nginx"
 exec "$@"
